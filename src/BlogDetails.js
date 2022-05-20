3d9e9b1caa56890397acbd1d5f2789475e2668f8
preethisun0801 <preethisun0801@gmail.com>
2022-05-20 14:41:24 +0530

    delete button

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,9 +1,13 @@
-import { useParams } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
 import useFetch from "./useFetch";
 
 const BlogDetails = () => {
     const {id} = useParams();
+    const history=useHistory();
     const {data:blog,loading,error}=useFetch('http://localhost:7000/blogs/'+id);
+    const handleClick = () =>{
+        fetch('http://localhost:7000/blogs/'+blog.id,{method:"DELETE"}).then(()=>{history.push("/")});
+    }
     return (
         <div className="blog-details">
             {error && <div>{error}</div>}
@@ -13,6 +17,7 @@ const BlogDetails = () => {
                     <h2>{blog.title}</h2>
                     <p>Author: {blog.author}</p>
                     <div>{blog.body}</div>
+                    <button onClick={handleClick}>Delete this blog</button>
                 </article>
             )}
         </div>
