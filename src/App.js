90ea1cee55d8d93a64c45a5e636d0927d8738273
preethisun0801 <preethisun0801@gmail.com>
2022-05-20 14:45:05 +0530

    404 page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './Navbar'
 import Home from './Home'
 import Create from './Create';
 import BlogDetails from './BlogDetails';
+import Error404 from './Error404';
 
 function App() {
   return (
@@ -19,6 +20,9 @@ function App() {
           </Route>
           <Route path="/blog/:id">
             <BlogDetails/>
+          </Route>
+          <Route path="*">
+            <Error404/>
           </Route>
         </Switch>
       </div>
