96310b3252b1921bb20fbdc6ad9888ee8bb773db
preethisun0801 <preethisun0801@gmail.com>
2022-05-20 13:50:37 +0530

    routing

---
+++ src/Create.js
@@ -0,0 +1,9 @@
+const Create = () => {
+    return (
+        <div className="create">
+            <h2>Add your blog here!</h2>
+        </div>
+    );
+}
+ 
+export default Create;
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,14 +1,15 @@
+import {Link} from 'react-router-dom';
 const Navbar = () => {
     return (
         <nav className="navbar">
             <h1>React Learning Blog</h1>
             <div className="links">
-                <a href="/">Home</a>
-                <a href="/create" style={{
+                <Link to="/">Home</Link>
+                <Link to="/create" style={{
                     color:"white",
                     backgroundColor:"#f1356d",
                     borderRadius:"10px"
-                }}>New Blog</a>
+                }}>New Blog</Link>
             </div>
         </nav>
     );
