6ffd3b0c01a5aeb85a0e2920dd6403dfdfcfadae
preethisun0801 <preethisun0801@gmail.com>
2022-05-20 13:36:45 +0530

    useFetch for reusability

--- src/Home.js
+++ src/Home.js
@@ -1,22 +1,7 @@
-import {useState,useEffect} from 'react';
+import useFetch from './useFetch';
 import BlogList from './BlogList';
 const Home = () => {
-    const [blogs,setBlogs] = useState(null);
-    const [loading,setLoading] = useState(true);
-    const [error,setError] = useState(true);
-    useEffect(()=>{
-        fetch('http://localhost:7000/blogs').then(res => {
-            if(!res.ok) {throw Error("Could not fetch the data for that resource.")}
-            return res.json();
-        }).then(data=>{
-            setBlogs(data);
-            setLoading(false);
-            setError(null);
-        }).catch(err=> {
-            setError(err.message)
-            setLoading(false);   
-        });
-    },[]);
+    const {data:blogs,loading,error}=useFetch('http://localhost:7000/blogs');
     return (
         <div className="home">
             {error && <div>{error}</div>}
---
+++ src/useFetch.js
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+
+const useFetch = (url) => {
+    const [data,setData] = useState(null);
+    const [loading,setLoading] = useState(true);
+    const [error,setError] = useState(true);
+    
+    useEffect(()=>{
+        fetch(url).then(res => {
+            if(!res.ok) {throw Error("Could not fetch the data for that resource.")}
+            return res.json();
+        }).then(data=>{
+            setData(data);
+            setLoading(false);
+            setError(null);
+        }).catch(err=> {
+            setError(err.message)
+            setLoading(false);   
+        });
+    },[]);
+    return {data,loading,error}
+}
+export default useFetch;
