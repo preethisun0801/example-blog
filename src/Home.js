f576a6d76cf122bfb9e4cbd33c9b911038c6d4bc
preethisun0801 <preethisun0801@gmail.com>
2022-05-20 13:30:16 +0530

    loading and error

--- src/Home.js
+++ src/Home.js
@@ -2,16 +2,25 @@ import {useState,useEffect} from 'react';
 import BlogList from './BlogList';
 const Home = () => {
     const [blogs,setBlogs] = useState(null);
-    
+    const [loading,setLoading] = useState(true);
+    const [error,setError] = useState(true);
     useEffect(()=>{
         fetch('http://localhost:7000/blogs').then(res => {
+            if(!res.ok) {throw Error("Could not fetch the data for that resource.")}
             return res.json();
         }).then(data=>{
             setBlogs(data);
-        })
+            setLoading(false);
+            setError(null);
+        }).catch(err=> {
+            setError(err.message)
+            setLoading(false);   
+        });
     },[]);
     return (
         <div className="home">
+            {error && <div>{error}</div>}
+            {loading && <div>Loading...</div>}
             {blogs && <BlogList blogs={blogs} title="All Blogs" />}
         </div>
     );
