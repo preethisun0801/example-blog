a01b75476cd9ab3d78ee80b76cd919db3bcd608d
preethisun0801 <preethisun0801@gmail.com>
2022-05-20 11:38:39 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
 
 function App() {
+  const title = "Welcome to the new blog"
   return (
     <div className="App">
-      
+      <div className="content">
+        <h1>{title}</h1>
+      </div>
     </div>
   );
 }
