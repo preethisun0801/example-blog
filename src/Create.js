c8efa5f60f2dc13e3f0b9022b911b77ff0c9a264
preethisun0801 <preethisun0801@gmail.com>
2022-05-20 14:37:18 +0530

    create blog and redirect to home once made

--- src/Create.js
+++ src/Create.js
@@ -1,9 +1,55 @@
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
 const Create = () => {
-    return (
-        <div className="create">
-            <h2>Add your blog here!</h2>
-        </div>
-    );
-}
- 
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("");
+  const [isPending, setIsPending] = useState(false);
+  const history = useHistory();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const blog = { title, body, author };
+    setIsPending(true);
+    fetch("http://localhost:7000/blogs", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(blog),
+    }).then(() => {
+      console.log("new blog added!");
+      setIsPending(false);
+      history.push("/");
+    });
+  };
+  return (
+    <div className="create">
+      <h2>Add your blog here!</h2>
+      <form onSubmit={handleSubmit}>
+        <label>Blog Title</label>
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        ></input>
+        <label>Blog Body</label>
+        <textarea
+          type="text"
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        ></textarea>
+        <label>Blog Author</label>
+        <input
+          type="text"
+          required
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+        ></input>
+        {!isPending && <button>Add Blog</button>}
+        {isPending && <button disabled>Adding Blog...</button>}
+      </form>
+    </div>
+  );
+};
+
 export default Create;
